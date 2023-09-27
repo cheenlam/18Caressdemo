a4c25469b985d208dff344b9e61b048155ee4420
redsinsue <redsinsue@gmail.com>
2023-09-27 09:55:42 +0800

    no message

--- js/main.js
+++ js/main.js
@@ -26,82 +26,82 @@ Vue.createApp({
             let sortType = '';
             let nowTitle = '熱門影片';
             // ==== 撈外部API ====
-            // switch (style) {
-            //     case 0: tagVal = '波多野結衣'; nowTitle = '波多野結衣'; break;
-            //     case 1: tagVal = '蜜美杏'; nowTitle = '蜜美杏'; break;
-            //     case 2: tagVal = '橋本ありな'; nowTitle = '橋本ありな'; break;
-            //     case 3: tagVal = '深田えいみ'; nowTitle = '深田えいみ'; break;
-            //     case 4: tagVal = '小早川怜子'; nowTitle = '小早川怜子'; break;
-            //     case 5: tagVal = '君島みお'; nowTitle = '君島みお'; break;
-            //     case 6: tagVal = '大槻ひびき'; nowTitle = '大槻ひびき'; break;
-            //     case 7: tagVal = '松下紗栄子'; nowTitle = '松下紗栄子'; break;
-            //     case 8: tagVal = '素人'; nowTitle = '鄰家素人'; break;
-            //     case 9: sortType = '3'; nowTitle = '西洋歐美'; break;
-            //     case 10: sortType = '4'; nowTitle = '裏番動漫'; break;
-            //     case 11: tagVal = '制服'; nowTitle = '制服誘惑'; break;
-            //     case 12: tagVal = '人妻'; nowTitle = '熟女人妻'; break;
-            // }
-            // axios.get("https://www.baline888.com/api.php/external/tagsel", { params: { key: "588THTUoI", tag: tagVal, type: sortType } }).then((r) => { this.mvList = r.data; this.nowList = nowTitle; });
+            switch (style) {
+                case 0: tagVal = '波多野結衣'; nowTitle = '波多野結衣'; break;
+                case 1: tagVal = '蜜美杏'; nowTitle = '蜜美杏'; break;
+                case 2: tagVal = '橋本ありな'; nowTitle = '橋本ありな'; break;
+                case 3: tagVal = '深田えいみ'; nowTitle = '深田えいみ'; break;
+                case 4: tagVal = '小早川怜子'; nowTitle = '小早川怜子'; break;
+                case 5: tagVal = '君島みお'; nowTitle = '君島みお'; break;
+                case 6: tagVal = '大槻ひびき'; nowTitle = '大槻ひびき'; break;
+                case 7: tagVal = '松下紗栄子'; nowTitle = '松下紗栄子'; break;
+                case 8: tagVal = '素人'; nowTitle = '鄰家素人'; break;
+                case 9: sortType = '3'; nowTitle = '西洋歐美'; break;
+                case 10: sortType = '4'; nowTitle = '裏番動漫'; break;
+                case 11: tagVal = '制服'; nowTitle = '制服誘惑'; break;
+                case 12: tagVal = '人妻'; nowTitle = '熟女人妻'; break;
+            }
+            axios.get("https://funsgirl.cc/api.php/external/tagsel", { params: { key: "588THTUoI", tag: tagVal, type: sortType } }).then((r) => { this.mvList = r.data; this.nowList = nowTitle; });
     
             // ==== 撈內部json ====
-            switch (style) {
-                case 0: 
-                    nowTitle = '波多野結衣'; 
-                    axios.get("json/actor_01.json").then((r) => { this.mvList = r.data; this.nowList = nowTitle; });
-                    break;
-                case 1: 
-                    nowTitle = '蜜美杏';
-                    axios.get("json/actor_02.json").then((r) => { this.mvList = r.data; this.nowList = nowTitle; });
-                    break;
-                case 2: 
-                    nowTitle = '橋本ありな'; 
-                    axios.get("json/actor_03.json").then((r) => { this.mvList = r.data; this.nowList = nowTitle; });
-                    break;
-                case 3: 
-                    nowTitle = '深田えいみ'; 
-                    axios.get("json/actor_04.json").then((r) => { this.mvList = r.data; this.nowList = nowTitle; });
-                    break;
-                case 4: 
-                    nowTitle = '小早川怜子'; 
-                    axios.get("json/actor_05.json").then((r) => { this.mvList = r.data; this.nowList = nowTitle; });
-                    break;
-                case 5: 
-                    nowTitle = '君島みお'; 
-                    axios.get("json/actor_06.json").then((r) => { this.mvList = r.data; this.nowList = nowTitle; });
-                    break;
-                case 6: 
-                    nowTitle = '大槻ひびき'; 
-                    axios.get("json/actor_07.json").then((r) => { this.mvList = r.data; this.nowList = nowTitle; });
-                    break;
-                case 7: 
-                    nowTitle = '松下紗栄子'; 
-                    axios.get("json/actor_08.json").then((r) => { this.mvList = r.data; this.nowList = nowTitle; });
-                    break;
-                case 8: 
-                    nowTitle = '鄰家素人'; 
-                    axios.get("json/sort_01.json").then((r) => { this.mvList = r.data; this.nowList = nowTitle; });
-                    break;
-                case 9: 
-                    nowTitle = '西洋歐美'; 
-                    axios.get("json/sort_02.json").then((r) => { this.mvList = r.data; this.nowList = nowTitle; });
-                    break;
-                case 10: 
-                    nowTitle = '裏番動漫'; 
-                    axios.get("json/sort_03.json").then((r) => { this.mvList = r.data; this.nowList = nowTitle; });
-                    break;
-                case 11: 
-                    nowTitle = '制服誘惑'; 
-                    axios.get("json/sort_04.json").then((r) => { this.mvList = r.data; this.nowList = nowTitle; });
-                    break;
-                case 12: 
-                    nowTitle = '熟女人妻'; 
-                    axios.get("json/sort_05.json").then((r) => { this.mvList = r.data; this.nowList = nowTitle; });
-                    break;
-                default:
-                    nowTitle = '熱門影片';
-                    axios.get("json/home.json").then((r) => { this.mvList = r.data; this.nowList = nowTitle; });
-                    break;
-            }
+            // switch (style) {
+            //     case 0: 
+            //         nowTitle = '波多野結衣'; 
+            //         axios.get("json/actor_01.json").then((r) => { this.mvList = r.data; this.nowList = nowTitle; });
+            //         break;
+            //     case 1: 
+            //         nowTitle = '蜜美杏';
+            //         axios.get("json/actor_02.json").then((r) => { this.mvList = r.data; this.nowList = nowTitle; });
+            //         break;
+            //     case 2: 
+            //         nowTitle = '橋本ありな'; 
+            //         axios.get("json/actor_03.json").then((r) => { this.mvList = r.data; this.nowList = nowTitle; });
+            //         break;
+            //     case 3: 
+            //         nowTitle = '深田えいみ'; 
+            //         axios.get("json/actor_04.json").then((r) => { this.mvList = r.data; this.nowList = nowTitle; });
+            //         break;
+            //     case 4: 
+            //         nowTitle = '小早川怜子'; 
+            //         axios.get("json/actor_05.json").then((r) => { this.mvList = r.data; this.nowList = nowTitle; });
+            //         break;
+            //     case 5: 
+            //         nowTitle = '君島みお'; 
+            //         axios.get("json/actor_06.json").then((r) => { this.mvList = r.data; this.nowList = nowTitle; });
+            //         break;
+            //     case 6: 
+            //         nowTitle = '大槻ひびき'; 
+            //         axios.get("json/actor_07.json").then((r) => { this.mvList = r.data; this.nowList = nowTitle; });
+            //         break;
+            //     case 7: 
+            //         nowTitle = '松下紗栄子'; 
+            //         axios.get("json/actor_08.json").then((r) => { this.mvList = r.data; this.nowList = nowTitle; });
+            //         break;
+            //     case 8: 
+            //         nowTitle = '鄰家素人'; 
+            //         axios.get("json/sort_01.json").then((r) => { this.mvList = r.data; this.nowList = nowTitle; });
+            //         break;
+            //     case 9: 
+            //         nowTitle = '西洋歐美'; 
+            //         axios.get("json/sort_02.json").then((r) => { this.mvList = r.data; this.nowList = nowTitle; });
+            //         break;
+            //     case 10: 
+            //         nowTitle = '裏番動漫'; 
+            //         axios.get("json/sort_03.json").then((r) => { this.mvList = r.data; this.nowList = nowTitle; });
+            //         break;
+            //     case 11: 
+            //         nowTitle = '制服誘惑'; 
+            //         axios.get("json/sort_04.json").then((r) => { this.mvList = r.data; this.nowList = nowTitle; });
+            //         break;
+            //     case 12: 
+            //         nowTitle = '熟女人妻'; 
+            //         axios.get("json/sort_05.json").then((r) => { this.mvList = r.data; this.nowList = nowTitle; });
+            //         break;
+            //     default:
+            //         nowTitle = '熱門影片';
+            //         axios.get("json/home.json").then((r) => { this.mvList = r.data; this.nowList = nowTitle; });
+            //         break;
+            // }
         },
         // 綁定videojs
         showVod(idx) {
